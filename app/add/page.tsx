9bfae2f4d9d515a630569e7b3d37f09bb078e30b
hukuryo <ryohei.1998.05.09@gmail.com>
2024-02-01 22:50:19 +0900

    ヘッダーとページを追加

---
+++ app/add/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ModeToggle } from "../../components/toggle";
+import { Header } from "@/components/header";
+
+export default function page() {
+  return (
+    <>
+      <Header />
+      <div className="text-center">
+        <p>add</p>
+      </div>
+    </>
+  );
+}
--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,19 @@
 
 import React from "react";
 import { ModeToggle } from "./toggle";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header>
-      <ModeToggle />
+    <header className="border-b py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-bg-gray-800 text-2xl font-semibold">TODO APP</h1>
+        <div className="flex items-center gap-5">
+          <Link href="/">List</Link>
+          <Link href="add">Add</Link>
+          <ModeToggle /> {/* モード切替コンポーネントの挿入 */}
+        </div>
+      </div>
     </header>
   );
 };
