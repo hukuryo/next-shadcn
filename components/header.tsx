9ad1ee3b5dc7af0685e2e10a910dc330f750b52b
hukuryo <91451759+hukuryo@users.noreply.github.com>
2024-02-02 17:08:36 +0900

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="border-b py-4">
+      
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-bg-gray-800 text-2xl font-semibold">TODO APP</h1>
         <div className="flex items-center gap-5">
